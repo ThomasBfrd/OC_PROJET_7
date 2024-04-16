640d854d29070ac2e288faf0e9555c57137aabca
ThomasBfrd <thomas.bfrd@gmail.com>
2024-04-16 22:05:05 +0200

    index js - instanciation des classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 import API from './api/Api.js';
 import RecetteCardBuilder from './pages/RecetteCardBuilder.js';
+import FiltersTags from './pages/FiltersTags.js';
+// import FiltersTags from './pages/FiltersTagsAlternative.js';
 import { triRecettes } from './utils/sortRecipesBySearching.js';
+// import { triRecettes } from './utils/sortRecipesBySearchingAlternative.js';
 
 const homepage = () => {
 	new API().getData().then(data => {
 		new RecetteCardBuilder().displayRecettes(data.recipes);
+		new FiltersTags().filterTags(data.recipes);
 		triRecettes(data.recipes);
 	}).catch(err => {
 		console.error(err);
